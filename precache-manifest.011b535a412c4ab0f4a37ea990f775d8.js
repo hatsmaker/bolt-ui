ca65275b3aa430aab235808cab43f570083ce3ae
artakpoghosyan <artakpoghosyan@workfront.com>
2020-05-16 00:36:29 +0400

    Updates

---
+++ precache-manifest.011b535a412c4ab0f4a37ea990f775d8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2266ab8314bba695e24091411dcd96cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdfb0a42ca8b65c70778",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d3c3dbd9570e2c2b5f2b",
+    "url": "/static/css/main.9126651d.chunk.css"
+  },
+  {
+    "revision": "cdfb0a42ca8b65c70778",
+    "url": "/static/js/2.6db7cab6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6db7cab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c3dbd9570e2c2b5f2b",
+    "url": "/static/js/main.f8465b53.chunk.js"
+  },
+  {
+    "revision": "639245d33797ccd17327",
+    "url": "/static/js/runtime-main.88d592a7.js"
+  },
+  {
+    "revision": "d8f8381564c42012f606ec0b6739bca8",
+    "url": "/static/media/arnamu_b.d8f83815.woff"
+  },
+  {
+    "revision": "e35c7e5747889fac6b1ca01731a94ee6",
+    "url": "/static/media/arnamu_bb.e35c7e57.woff2"
+  }
+]);
