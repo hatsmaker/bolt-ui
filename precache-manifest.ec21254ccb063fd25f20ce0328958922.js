64d1489a714f5b14d11985f98ff9bc5c66dffb17
artakpoghosyan <artakpoghosyan@workfront.com>
2020-05-16 01:46:43 +0400

    Updates

---
+++ precache-manifest.ec21254ccb063fd25f20ce0328958922.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f1049662539a1d45b0ad0f67dcc355",
+    "url": "/bolt/index.html"
+  },
+  {
+    "revision": "15d7c0f253a5271ba566",
+    "url": "/bolt/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a465ba8f56efd6106e8f",
+    "url": "/bolt/static/css/main.116444eb.chunk.css"
+  },
+  {
+    "revision": "15d7c0f253a5271ba566",
+    "url": "/bolt/static/js/2.80bdd03b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bolt/static/js/2.80bdd03b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a465ba8f56efd6106e8f",
+    "url": "/bolt/static/js/main.3309a872.chunk.js"
+  },
+  {
+    "revision": "2cb48fe687bc200fbf7d",
+    "url": "/bolt/static/js/runtime-main.60d2c75f.js"
+  },
+  {
+    "revision": "d8f8381564c42012f606ec0b6739bca8",
+    "url": "/bolt/static/media/arnamu_b.d8f83815.woff"
+  },
+  {
+    "revision": "e35c7e5747889fac6b1ca01731a94ee6",
+    "url": "/bolt/static/media/arnamu_bb.e35c7e57.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hatsagortsyan12/hatsagortsyan12.github.io.git/precache-manifest.30dd36c308c407514a0f5ebc86de77d5.js"
+  "/bolt/precache-manifest.ec21254ccb063fd25f20ce0328958922.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hatsagortsyan12/hatsagortsyan12.github.io.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bolt/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
